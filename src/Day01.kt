b2c764a761508e7a6cbfad8ad76b6562b727ebbf
JaapBeetstra <jaap@app-it-up.com>
2024-12-01 12:47:17 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,27 @@
+import kotlin.math.absoluteValue
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val lines = linesAsIntArrays(input)
+        val nrs1 = lines.map { it[0] }.sorted()
+        val nrs2 = lines.map { it[1] }.sorted()
+        return nrs1.zip(nrs2).sumOf { (a, b) -> (a - b).absoluteValue }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val lines = linesAsIntArrays(input)
+        val nrs1 = lines.map { it[0] }
+        val nrs2 = lines.map { it[1] }.groupingBy { it }.eachCount()
+        return nrs1.sumOf { it.toLong() * (nrs2[it] ?: 0) }
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
+    check(part2(testInput) == 31L)
     part2(input).println()
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,7 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun linesAsIntArrays(input: List<String>) = input.filter { it.isNotBlank() }.map { line ->
+    line.split(Regex("\\s+")).map { it.toInt() }.toIntArray()
+}
