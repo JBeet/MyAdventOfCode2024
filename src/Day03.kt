473193d2dec2a241b18bbde59fa7cc1dcac2a1ff
JaapBeetstra <jaap@app-it-up.com>
2024-12-03 08:54:41 +0100

    Day 03

---
+++ src/Day03.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val test = """xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"""
+    check(Day03.part1(test) == 161)
+    val input = readInput("Day03")
+    println(Day03.part1(input))
+    val test2 = """xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"""
+    check(Day03.part2(test2) == 48)
+    println(Day03.part2(input))
+}
+
+object Day03 {
+    fun part1(input: String): Int = Regex("""mul\((\d{1,3}),(\d{1,3})\)""").findAll(input).map { it.destructured }
+        .sumOf { (a, b) -> a.toInt() * b.toInt() }
+
+    fun part2(input: String): Int = input.split("do()").map { it.substringBefore("don't()") }.sumOf { part1(it) }
+}
